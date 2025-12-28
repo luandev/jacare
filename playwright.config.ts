c6a771b20f52553a1736cf13286163ef0936e224
luan <luan.dev@gmail.com>
2025-12-28 11:24:47 +0000

    Update webServer command to build shared package before starting tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run build -w @crocdesk/web && npm run dev:server',
+    command: 'npm run build -w @crocdesk/shared && npm run build -w @crocdesk/web && npm run dev:server',
     url: 'http://localhost:3333',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
